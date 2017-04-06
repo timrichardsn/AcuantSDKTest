da747eb124786185c17f45cb616741e37a1c08b1
Tim Richardson <timjonrichardson@gmail.com>
2017-04-06 10:35:24 +0200

    Merge to master (#1)

---
+++ AcuantSwiftTest/AcuantSwiftTest-BridgingHeader.h
@@ -0,0 +1,32 @@
+//
+//  AcuantSwiftTest-BridgingHeader.h
+//  AcuantSwiftTest
+//
+//  Created by Tim Richardson on 06/04/2017.
+//  Copyright © 2017 TRCO. All rights reserved.
+//
+
+#ifndef AcuantSwiftTest_BridgingHeader_h
+#define AcuantSwiftTest_BridgingHeader_h
+
+
+#endif /* AcuantSwiftTest_BridgingHeader_h */
+
+@protocol AcuantFacialCaptureDelegate;
+@class AcuantMobileSDKController;
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "AcuantMobileSDK/AcuantCardProcessRequestOptions.h"
+#import "AcuantMobileSDK/AcuantCardRegion.h"
+#import "AcuantMobileSDK/AcuantCardResult.h"
+#import "AcuantMobileSDK/AcuantCardType.h"
+#import "AcuantMobileSDK/AcuantDeviceLocationTestResult.h"
+#import "AcuantMobileSDK/AcuantDriversLicenseCard.h"
+#import "AcuantMobileSDK/AcuantError.h"
+#import "AcuantMobileSDK/AcuantFacialCaptureDelegate.h"
+#import "AcuantMobileSDK/AcuantFacialData.h"
+#import "AcuantMobileSDK/AcuantFacialRecognitionViewController.h"
+#import "AcuantMobileSDK/AcuantMedicalInsuranceCard.h"
+#import "AcuantMobileSDK/AcuantMobileSDKController.h"
+#import "AcuantMobileSDK/AcuantPassaportCard.h"
---
+++ AcuantSwiftTest/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  AcuantSwiftTest
+//
+//  Created by Tim Richardson on 06/04/2017.
+//  Copyright © 2017 TRCO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var instance:AcuantMobileSDKController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        instance = AcuantMobileSDKController.initAcuantMobileSDK(withLicenseKey: "", andDelegate: self)
+    }
+    
+    @IBAction func openPassportScanner(_ sender: Any) {
+        instance?.showManualCameraInterface(in: self, delegate: self, cardType: AcuantCardTypePassportCard, region: AcuantCardRegionEurope, andBackSide: false)
+    }
+}
+
+// MARK: - AcuantMobileSDKControllerCapturingDelegate
+extension ViewController : AcuantMobileSDKControllerCapturingDelegate {
+    func didCaptureCropImage(_ cardImage: UIImage!, scanBackSide: Bool) {
+        print("didCaptureCropImage")
+        instance?.dismissCardCaptureInterface()
+    }
+    
+    func didCaptureOriginalImage(_ cardImage: UIImage!) {
+        print("didCaptureOriginalImage")
+    }
+    
+    func didCaptureCropImage(_ cardImage: UIImage!, andData data: String!, scanBackSide: Bool) {
+        print("didCaptureCropImage and data")
+    }
+    
+    // called when capturing the barcode for a drivers license when using the barcode CaptureMethod (see start())
+    func didCaptureData(_ data: String!) {
+        print("didCaptureData \(data)")
+    }
+    
+    func didFailWithError(_ error: AcuantError!) {
+        print("didFailWithError: type: \(error.errorType) message: \(error.errorMessage)")
+        
+        switch error.errorType {
+        case AcuantErrorInvalidLicenseKey:
+            // need to coordinate an effort to listen for a network connection, at which point we can re activate
+            break
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - AcuantMobileSDKControllerProcessingDelegate
+extension ViewController : AcuantMobileSDKControllerProcessingDelegate {
+    func didFinishProcessingCard(with result: AcuantCardResult!) {
+        print("didFinishProcessingCard")
+    }
+    
+    func didFinishValidatingImage(with result: AcuantCardResult!) {
+        print("didFinishValidatingImage")
+    }
+}
