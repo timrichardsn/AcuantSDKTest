c718c3c2de826c201e930b16f902ebf50e5f56a4
Tim Richardson <tim@timrichardson.co>
2017-04-06 09:44:35 +0200

    Added basic setup for demonstration purposes

--- AcuantSwiftTest/AcuantSwiftTest/ViewController.swift
+++ AcuantSwiftTest/AcuantSwiftTest/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var instance:AcuantMobileSDKController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        instance = AcuantMobileSDKController.initAcuantMobileSDK(withLicenseKey: "F9DB7D13F8CF", andDelegate: self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func openPassportScanner(_ sender: Any) {
+        instance?.showManualCameraInterface(in: self, delegate: self, cardType: AcuantCardTypePassportCard, region: AcuantCardRegionEurope, andBackSide: false)
     }
-
-
 }
 
+// MARK: - AcuantMobileSDKControllerCapturingDelegate
+extension ViewController : AcuantMobileSDKControllerCapturingDelegate {
+    func didCaptureCropImage(_ cardImage: UIImage!, scanBackSide: Bool) {
+        print("didCaptureCropImage")
+        instance?.dismissCardCaptureInterface()
+    }
+    
+    func didCaptureOriginalImage(_ cardImage: UIImage!) {
+        print("didCaptureOriginalImage")
+    }
+    
+    // called when capturing the barcode for a drivers license when using the barcode CaptureMethod (see start())
+    func didCaptureData(_ data: String!) {
+        print("didCaptureData \(data)")
+    }
+    
+    func didFailWithError(_ error: AcuantError!) {
+        print("didFailWithError: type: \(error.errorType) message: \(error.errorMessage)")
+        
+        switch error.errorType {
+        case AcuantErrorInvalidLicenseKey:
+            // need to coordinate an effort to listen for a network connection, at which point we can re activate
+            break
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - AcuantMobileSDKControllerProcessingDelegate
+extension ViewController : AcuantMobileSDKControllerProcessingDelegate {
+    func didFinishProcessingCard(with result: AcuantCardResult!) {
+        print("didFinishProcessingCard")
+    }
+    
+    func didFinishValidatingImage(with result: AcuantCardResult!) {
+        print("didFinishValidatingImage")
+    }
+}
